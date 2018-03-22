e2874a9a75ccb54769b66d40a5d848046433e4a3
camilo17 <cami172013@gmail.com>
2018-03-22 16:52:35 -0500

    Created the permission js.

---
+++ permissions.js
@@ -0,0 +1,19 @@
+const createResolver = resolver => {
+  const baseResolver = resolver;
+  baseResolver.createResolver = childResolver => {
+    const newResolver = async (parent, args, context, info) => {
+      await resolver(parent, args, context, info);
+      return childResolver(parent, args, context, info);
+    };
+    return createResolver(newResolver);
+  };
+  return baseResolver;
+};
+
+const requiresAuth = createResolver((parent, args, context) => {
+  if (!context.user || !context.user.id) {
+    throw new Error("Not authenticated");
+  }
+});
+
+export default requiresAuth;
