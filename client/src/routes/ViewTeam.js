97841e41a873755d181b621921d7b6be1f285a6d
camilo17 <cami172013@gmail.com>
2018-03-25 19:39:24 -0500

    Fixed another bug with the view team, we parsed the channelId and teamId to make sure they were integers so the site doesn't crash if users put in random paramaters in url.

--- client/src/routes/ViewTeam.js
+++ client/src/routes/ViewTeam.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "react-apollo";
 import { gql } from "apollo-boost";
+import { Redirect } from "react-router-dom";
 import Header from "../components/Header";
 import Messages from "../components/Messages";
 import SendMessage from "../components/SendMessage";
@@ -16,12 +17,21 @@ const ViewTeam = ({
     return null;
   }
 
-  const teamIdx = teamId
-    ? findIndex(allTeams, ["id", parseInt(teamId, 10)])
+  if (!allTeams.length) {
+    return <Redirect to="/create-team" />;
+  }
+
+  const teamIdInteger = parseInt(teamId, 10);
+
+  const teamIdx = teamIdInteger
+    ? findIndex(allTeams, ["id", teamIdInteger])
     : 0;
   const team = allTeams[teamIdx];
-  const channelIdx = channelId
-    ? findIndex(team.channels, ["id", parseInt(channelId, 10)])
+
+  const channelIdInteger = parseInt(channelId, 10);
+
+  const channelIdx = channelIdInteger
+    ? findIndex(team.channels, ["id", channelIdInteger])
     : 0;
   const channel = team.channels[channelIdx];
 
