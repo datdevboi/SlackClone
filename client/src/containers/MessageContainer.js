819dc1efd52acd42024000c879570feacab61c79
camilo17 <cami172013@gmail.com>
2018-03-27 15:23:08 -0500

    Worked on the messages resolver to fetch messages and then display them on the screen.

--- client/src/containers/MessageContainer.js
+++ client/src/containers/MessageContainer.js
@@ -2,9 +2,26 @@ import React from "react";
 import Messages from "../components/Messages";
 import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
+import { Comment } from "semantic-ui-react";
 
 const MessageContainer = ({ data: { loading, messages } }) =>
-  loading ? null : <Messages>{JSON.stringify(messages)}</Messages>;
+  loading ? null : (
+    <Messages>
+      <Comment.Group>
+        {messages.map(m => (
+          <Comment key={`${m.id}-message`}>
+            <Comment.Content>
+              <Comment.Author>{m.user.username}</Comment.Author>
+              <Comment.Metadata>
+                <div>{m.created_at}</div>
+              </Comment.Metadata>
+              <Comment.Text>{m.text}</Comment.Text>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
+    </Messages>
+  );
 
 const messagesQuery = gql`
   query($channelId: Int!) {
@@ -14,7 +31,7 @@ const messagesQuery = gql`
       user {
         username
       }
-      createdAt
+      created_at
     }
   }
 `;
--- resolvers/message.js
+++ resolvers/message.js
@@ -1,17 +1,40 @@
+import requiresAuth from "../permissions";
+
 export default {
+  Message: {
+    user: ({ userId }, args, { models }) =>
+      models.User.findOne({ where: { id: userId } })
+  },
   Query: {
-    messages: async (parent, args, { models, user }, info) => []
+    messages: requiresAuth.createResolver(
+      async (parent, { channelId }, { models, user }, info) => {
+        const messages = await models.Message.findAll(
+          {
+            order: [["created_at", "ASC"]]
+          },
+          {
+            where: { channelId }
+          },
+          {
+            raw: true
+          }
+        );
+        return messages;
+      }
+    )
   },
   Mutation: {
-    createMessage: async (parent, args, context, info) => {
-      const { models, user } = context;
-      try {
-        await models.Message.create({ ...args, userId: user.id });
-        return true;
-      } catch (error) {
-        console.log(error);
-        return false;
+    createMessage: requiresAuth.createResolver(
+      async (parent, args, context, info) => {
+        const { models, user } = context;
+        try {
+          await models.Message.create({ ...args, userId: user.id });
+          return true;
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
       }
-    }
+    )
   }
 };
